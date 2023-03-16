9823cdf740ae43ff0abd623348dd77be713bf139
saihariniii <harini9121@gmail.com>
2023-03-16 21:22:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let headTitEl = document.getElementById("header-title");
+let addItemEl = document.getElementById("items");
+
+headTitEl.style.border = "2px solid black";
+addItemEl.style.color="Green";
+addItemEl.style.fontWeight="bold";
