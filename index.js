be30a0fbd8fa0036eaf27eaa3dce18bf504f0609
saihariniii <harini9121@gmail.com>
2023-03-25 11:19:24 +0530

    tag commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const collection = document.getElementsByClassName("list-group-item");
+const collection = document.getElementsByTagName("li");
 for (let i = 0; i < collection.length; i++) {
-  if(i==2){
+  if(i==4){
     collection[i].style.backgroundColor = "green";
   }else{
     collection[i].style.backgroundColor = "red";
     collection[i].style.fontWeight="bold";
   }
 }
-
